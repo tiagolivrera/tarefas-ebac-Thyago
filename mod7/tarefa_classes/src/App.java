fbf30157b689f40f1c569fb74c38bae5527fe2bd
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-18 16:42:52 -0300

    Entrega: Exercício Módulo 7

---
+++ mod7/tarefa_classes/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Gato rex = new Gato();
+
+        rex.miar();
+        rex.andar();
+        rex.voar();
+    }
+}
---
+++ mod7/tarefa_classes/src/Gato.java
@@ -0,0 +1,16 @@
+public class Gato {
+    public String nome;
+    public Integer idade;
+
+    public void miar() {
+        System.out.println("miar(): Miau");
+    }
+
+    public void andar() {
+        System.out.println("andar(): Andando...");
+    }
+
+    public void voar() {
+        System.out.println("voar(): ERRO! Ainda não suporto essa função :(");
+    }
+}
