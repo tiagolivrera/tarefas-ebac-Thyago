f5d4bf3f653eb89975a73447350d201e6a778864
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-18 16:45:25 -0300

    Add comentarios na classe Gato (mod7)

--- mod7/tarefa_classes/src/Gato.java
+++ mod7/tarefa_classes/src/Gato.java
@@ -1,15 +1,22 @@
 public class Gato {
-    public String nome;
-    public Integer idade;
+    /*
+     * Classe que representa um gato
+     * versao: 1.0
+     */
+    public String nome;         // nome do gato
+    public Integer idade;       // idade do gato
 
+    // metodo que faz o gato voar
     public void miar() {
         System.out.println("miar(): Miau");
     }
 
+    // metodo que faz o gato andar
     public void andar() {
         System.out.println("andar(): Andando...");
     }
 
+    // metodo que faz o gato voar (ou quase)
     public void voar() {
         System.out.println("voar(): ERRO! Ainda não suporto essa função :(");
     }
