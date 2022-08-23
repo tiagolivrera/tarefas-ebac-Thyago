6f2279506709e6326789ba2399a1bc9f15f7ed36
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-22 22:23:19 -0300

    Exercicios do modulo 8 -- recebendo dados do console

--- mod8/tarefa_calculo_media/src/App.java
+++ mod8/tarefa_calculo_media/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Média das notas 5.0, 6.0, 7.0 e 8.0: " + Media.media_4notas(5, 6, 7, 8));
+        double a, b, c, d;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma nota: ");
+        a = sc.nextDouble();
+        System.out.print("Digite uma nota: ");
+        b = sc.nextDouble();
+        System.out.print("Digite uma nota: ");
+        c = sc.nextDouble();
+        System.out.print("Digite uma nota: ");
+        d = sc.nextDouble();
+        sc.close();
+
+        System.out.println("Média das notas 5.0, 6.0, 7.0 e 8.0: " + Media.media_4notas(a, b, c, d));
     }
 }
