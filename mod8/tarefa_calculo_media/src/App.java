3951f6eabf5c10494581ea17729f5c440b98c631
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-22 21:57:22 -0300

    Exercicios do modulo 8

---
+++ mod8/tarefa_calculo_media/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Média das notas 5.0, 6.0, 7.0 e 8.0: " + Media.media_4notas(5, 6, 7, 8));
+    }
+}
