661481d3eaa392958fc74b0a25b8b77c3e8a42b0
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-03-10 09:32:12 -0300

    Upload exercicio mod 18

---
+++ mod18/tarefa_annotation/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Tabela(value = "nome_da_tabela")
+    public void cria_tabela() {
+
+    }
+}
