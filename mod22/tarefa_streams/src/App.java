d11117c52d71c81b45eb85f92493edd75ff5258b
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-03-13 20:58:34 -0300

    Correcao exercicio mod 22

--- mod22/tarefa_streams/src/App.java
+++ mod22/tarefa_streams/src/App.java
@@ -1,10 +1,20 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        List<Pessoa> lista = Pessoa.listaPessoas();
+        List<Pessoa> lista = Pessoa.listaPessoas().stream().collect(Collectors.toList());
+
+        System.out.print("Digite a quantidade de pessoas a serem inseridas: ");
+        int pessoas = Integer.parseInt(System.console().readLine());
+        
+        for (int i = 0; i < pessoas; i++) {
+            lista.add(Pessoa.stringToPessoa(System.console().readLine()));
+        }
 
         Stream<Pessoa> stream = lista.stream()
                 .filter(p -> Character.toString(p.getSexo()).equals("F"));
