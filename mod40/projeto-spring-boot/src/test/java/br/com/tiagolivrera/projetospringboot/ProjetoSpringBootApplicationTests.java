b293d694829bc9fb5b0f60a1644d56c08fbcfa54
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-03-19 17:30:59 -0300

    Upload exercicio mod 40

---
+++ mod40/projeto-spring-boot/src/test/java/br/com/tiagolivrera/projetospringboot/ProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagolivrera.projetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
